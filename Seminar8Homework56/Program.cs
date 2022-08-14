ba8bab04004ea72da796ed7d23e642b440c9307a
Yana Demidovich <demyanvo1990@gmail.com>
2022-08-14 23:22:34 +0300

    Сохранила домашнее задание: задача 56

---
+++ Seminar8Homework56/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке 
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) // метод, который задает 2D массив
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < newArray.GetLength(0); i++)
+        for (int j = 0; j < newArray.GetLength(1); j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue) + 1;
+
+    return newArray;
+}
+
+void PrintArray(int[,] array) // метод, который выводит массив на экран
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// дописать метод, который 
+// 1. пронумерует строки
+// 2. найдет сумму элементов каждой строки
+// 3. сравнит полученные суммы
+// 4. выведет номер строки, у которой сумма меньше
+
+Console.WriteLine("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите min значение: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите max значение: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Console.WriteLine();
+PrintArray(myArray);
