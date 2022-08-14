4153c78a1005a52d23fc5db2f00d4d575d0c3fa5
Yana Demidovich <demyanvo1990@gmail.com>
2022-08-14 23:34:38 +0300

    Сохранила домашнее задание: задача 56

--- Seminar8Homework56/Program.cs
+++ Seminar8Homework56/Program.cs
@@ -8,44 +8,63 @@
 // Программа считает сумму элементов в каждой строке 
 // и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
-int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) // метод, который задает 2D массив
+int[,] CreateArray(int rows, int columns, int minValue, int maxValue) // метод создания 2D массива
 {
     int[,] newArray = new int[rows, columns];
 
-    for (int i = 0; i < newArray.GetLength(0); i++)
-        for (int j = 0; j < newArray.GetLength(1); j++)
-            newArray[i, j] = new Random().Next(minValue, maxValue) + 1;
-
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue + 1);
     return newArray;
 }
 
-void PrintArray(int[,] array) // метод, который выводит массив на экран
+void ShowArray(int[,] array) // метод вывода массива на экран
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
             Console.Write(array[i, j] + " ");
-
         Console.WriteLine();
     }
-    Console.WriteLine();
 }
 
-// дописать метод, который 
-// 1. пронумерует строки
-// 2. найдет сумму элементов каждой строки
-// 3. сравнит полученные суммы
-// 4. выведет номер строки, у которой сумма меньше
+string FindSumRows (int [,] value) // метод поиска суммы значений в строках
+{
+    int result = 0;
+    int count = 1;
+    for (int i = 0; i < value.GetLength (0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < value.GetLength(1); j++)
+        {
+            sum += value [i, j];
+        }
+        //Console.Write(sum + " ");
+        if (i == 0)
+        {
+            result = sum;
+            count = i + 1;
+        }
+        if (i > 0 && sum < result)
+        {
+            result = sum;
+            count = i + 1;
+        }
+    }
+    return "строка " + count.ToString();
+}
 
-Console.WriteLine("Введите количество строк: ");
+Console.WriteLine("Input number of rows");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов: ");
+Console.WriteLine("Input number of columns");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите min значение: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите max значение: ");
-int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of min");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of max");
+int maxValue = Convert.ToInt32(Console.ReadLine());
 
-int[,] myArray = CreateRandom2dArray(m, n, min, max);
+int[,] myarray = CreateArray(m, n, minValue, maxValue);
+ShowArray(myarray);
 Console.WriteLine();
-PrintArray(myArray);
+FindSumRows(myarray);
+Console.WriteLine(FindSumRows(myarray));
