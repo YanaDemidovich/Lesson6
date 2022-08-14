edc2b4234e61e8193b3f718d35b754080b0ad693
Yana Demidovich <demyanvo1990@gmail.com>
2022-08-14 22:58:40 +0300

    Сохранила семинар

---
+++ Seminar8/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] newArray = new int[rows, columns];
+
+    for (int i = 0; i < newArray.GetLength(0); i++)
+        for (int j = 0; j < newArray.GetLength(1); j++)
+            newArray[i, j] = new Random().Next(minValue, maxValue) + 1;
+
+    return newArray;        
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int[,] ReversMatrix(int[,] array)
+{
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("Uncirrect matrix format!");
+        return array;
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = i + 1; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+    return array;
+}
+
+
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of colums: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+Show2dArray(ReversMatrix(myArray));
