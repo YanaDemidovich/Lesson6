09a616a7daa91310588adc8a434548b1ed83a6cc
Yana Demidovich <demyanvo1990@gmail.com>
2022-08-15 22:12:46 +0300

    Сохранила задачу 64

---
+++ Seminar9Homework64/Program.cs
@@ -0,0 +1,20 @@
+﻿// === Задача 64 === Задайте значение N. 
+// Напишите программу, которая найдет кол-во цифр 
+// в числе N рекурсивным методом.
+// N = 4532 -> 4
+
+Console.Clear();
+
+int Recur(int n)// метод рекурсивный
+{
+    return n < 10 ? 1 : 1 + Recur(n / 10);
+}
+void Print(string[] args)// метод вывода на печать
+{
+    Console.Write("Введите n=");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Recur(n));
+    Console.ReadKey(true);
+}
+
+Print(args);
